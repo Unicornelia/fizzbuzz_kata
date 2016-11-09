9ead3f0a3fa97e210872965f27d1a80fdc86ffe9
unicornelia <ez.kornelia@gmail.com>
2016-11-09 09:55:01 +0000

    Updating fizzbuzz with more testing and new lines of code Wednesday coding session in the morning

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,12 @@
-def fizzbuzz(number)
-  if (number == 15)
-    "fizzbuzz"
-  elsif number == 5
-    "buzz"
-  elsif number == 3
-    "fizz"
-  else
-    number
-  end
+def is_devisible_by_3?(number)
+  number % 3 == 0
+end
+
+def is_devisible_by_5?(number)
+  number % 5 == 0
+end
+
+def is_devisible_by_15?(number)
+  number % 15 == 0
+  #(number % 3 == 0) && (number % 5 == 0)
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,19 +1,23 @@
-require "fizzbuzz"
+require "./lib/fizzbuzz"
 
 describe "fizzbuzz" do
-  it "returns fizzbuzz when given 15" do
-    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  it "Tests whether number can be divided by 3" do
+    expect(is_devisible_by_3?(3)).to eq true
   end
-end
 
-describe "fizzbuzz" do
-  it "returns fizz when given 3" do
-    expect(fizzbuzz(3)).to eq "fizz"
+  it "Tests whether number can be divided by 3" do
+    expect(is_devisible_by_3?(4)).to eq false
   end
-end
 
-describe "fizzbuzz" do
-  it "returns buzz when given 5" do
-    expect(fizzbuzz(5)).to eq "buzz"
+  it "Tests whether number can be divided by 5" do
+    expect(is_devisible_by_5?(5)).to eq true
+  end
+
+  it "Tests whether number can be divided by 5" do
+    expect(is_devisible_by_5?(6)).to eq false
+  end
+
+  it "Tests whether number can be divided by 15" do
+    expect(is_devisible_by_15?(15)).to eq true
   end
 end
