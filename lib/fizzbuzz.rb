096379e54e2e80a9e85f387cde009a0db1c64274
unicornelia <ez.kornelia@gmail.com>
2016-11-09 10:03:55 +0000

    Refractoring once again and adding even more tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -10,3 +10,7 @@ def is_devisible_by_15?(number)
   number % 15 == 0
   #(number % 3 == 0) && (number % 5 == 0)
 end
+
+def is_devisible_by?(number, divisor)
+  number % divisor == 0
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -20,4 +20,16 @@ describe "fizzbuzz" do
   it "Tests whether number can be divided by 15" do
     expect(is_devisible_by_15?(15)).to eq true
   end
+
+  it "Tests whether number can be devided arbitrary" do
+    expect(is_devisible_by?(25, 5)).to eq true
+    expect(is_devisible_by?(9, 3)).to eq true
+    expect(is_devisible_by?(16, 4)).to eq true
+  end
+
+  it "Tests whther number can be devided abritrary?" do
+    expect(is_devisible_by?(17, 4)).to eq false
+    expect(is_devisible_by?(19, 5)).to eq false
+    expect(is_devisible_by?(23, 17)).to eq false
+  end
 end
